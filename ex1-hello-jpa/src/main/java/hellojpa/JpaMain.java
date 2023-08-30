ad95930908864b3f9e836ea6cd8030e92a332b35
TeTedo <qkrxotjr9508@gmail.com>
2023-08-30 21:23:09 +0900

    study jpa - persist

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,12 @@ public class JpaMain {
         tx.begin();
 
         try{
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(150L, "A");
 
-            for (Member member : result) {
-                System.out.println("member = " + member.getName());
-            }
+            em.persist(member1);
+            em.persist(member2);
+
 
             tx.commit();
         }catch (Exception e) {
