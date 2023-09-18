a9dfb6e9c05ef2bee9568ecab853d002b3bd7e09
TeTedo <qkrxotjr9508@gmail.com>
2023-09-19 00:08:01 +0900

    study : query v6

--- jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
     @JsonIgnore
@@ -18,6 +20,15 @@ public class OrderQueryDto {
     private OrderStatus orderStatus;
     private Address address;
     private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
 
     public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
         this.orderId = orderId;
