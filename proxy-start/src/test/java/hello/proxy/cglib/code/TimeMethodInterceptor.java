bca160cff300031ab059710ed621d3af1d6196d1
박태석 <qkrxotjr9508@gmail.com>
2023-11-26 23:19:50 +0900

    study : cglib proxy

--- proxy-start/src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
+++ proxy-start/src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
@@ -1,25 +1,26 @@
-package hello.proxy.jdkdynamic.code;
+package hello.proxy.cglib.code;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
 
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 @Slf4j
-public class TimeInvocationHandler implements InvocationHandler {
+public class TimeMethodInterceptor implements MethodInterceptor {
 
     private final Object target;
 
-    public TimeInvocationHandler(Object target) {
+    public TimeMethodInterceptor(Object target) {
         this.target = target;
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
         log.info("TImeProxy 실행");
         long startTime = System.currentTimeMillis();
 
-        Object result = method.invoke(target, args);
+        Object result = methodProxy.invoke(target, args);
 
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
